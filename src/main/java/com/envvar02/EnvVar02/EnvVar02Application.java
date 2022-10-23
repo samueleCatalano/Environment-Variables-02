f52a3784946b36015a6e1ef0eb6715bfb923fac0
Samuele Catalano <csamuele73@mail.com>
2022-10-23 17:16:05 +0200

    first commit

---
+++ src/main/java/com/envvar02/EnvVar02/EnvVar02Application.java
@@ -0,0 +1,13 @@
+package com.envvar02.EnvVar02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvVar02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvVar02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/envvar02/EnvVar02/EnvVar02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.envvar02.EnvVar02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvVar02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
